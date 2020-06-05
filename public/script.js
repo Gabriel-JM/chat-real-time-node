6dac1a325350a4cc0fdcd19644f4d23a60cace4c
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-06-05 13:51:28 -0300

    Add form events and socket sendMessage and receiveMessage events

--- public/script.js
+++ public/script.js
@@ -1,1 +1,32 @@
 const socket = io('http://localhost:3000')
+const chat = document.querySelector('#chat')
+
+chat.addEventListener('submit', sendMessage)
+
+socket.on('receivedMessage', message => renderMessage(message))
+socket.on('previousMessages', messages => messages.forEach(message => renderMessage(message)))
+
+function sendMessage(event) {
+  event.preventDefault()
+  const author = chat.username.value
+  const message = chat.message.value
+
+  if(author && message) {
+    const messageToSend = {
+      author,
+      message
+    }
+
+    renderMessage(messageToSend)
+    socket.emit('sendMessage', messageToSend)
+  }
+}
+
+function renderMessage(message) {
+  const messagesDiv = document.querySelector('.message')
+  const messageElement = document.createElement('div')
+  messageElement.className = 'message-container'
+  messageElement.innerHTML = `<strong>${message.author}</strong>: ${message.message}`
+
+  messagesDiv.appendChild(messageElement)
+}
