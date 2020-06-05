974bcfa64acfc43ce7745754348347927246dfdf
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-06-05 12:30:24 -0300

    Adding server file, where magic happens

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const socketIO = require('socket.io')
+const ejs = require('ejs')
+const path = require('path')
+const http = require('http')
+
+const app = express()
+const httpServer = http.createServer(app)
+const io = socketIO(httpServer)
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', ejs.renderFile)
+app.set('view engine', 'html')
+
+app.use('/', (req, res) => {
+  res.render('index.html')
+})
+
+httpServer.listen(3000, console.log('Server running'))
