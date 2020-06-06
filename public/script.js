68eeb6f8646016fdf6e0fbf196d0916bf30c1729
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-06-05 23:16:12 -0300

    Some styling

--- public/script.js
+++ public/script.js
@@ -4,7 +4,9 @@ const chat = document.querySelector('#chat')
 chat.addEventListener('submit', sendMessage)
 
 socket.on('receivedMessage', message => renderMessage(message))
-socket.on('previousMessages', messages => messages.forEach(message => renderMessage(message)))
+socket.on('previousMessages', messages => {
+  messages.forEach(message => renderMessage(message))
+})
 
 function sendMessage(event) {
   event.preventDefault()
@@ -26,7 +28,7 @@ function renderMessage(message) {
   const messagesDiv = document.querySelector('.message')
   const messageElement = document.createElement('div')
   messageElement.className = 'message-container'
-  messageElement.innerHTML = `<strong>${message.author}</strong>: ${message.message}`
+  messageElement.innerHTML = `<strong>${message.author}:</strong> ${message.message}`
 
   messagesDiv.appendChild(messageElement)
 }
